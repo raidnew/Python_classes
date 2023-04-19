46e3f4fdcbc6d5e97e6229c01e10cd7059021545
o.toronov <o.toronov@stc-spb.ru>
2023-04-19 14:53:17 +0300

    *fix, add recursion

--- task34.py
+++ task34.py
@@ -2,7 +2,12 @@
 #  Напишите рекурсивную функцию sumn(n), которая вычисляет и печатает сумму чисел от 0 до n.
 from functools import reduce
 
-def sumn(n):
+def sumn_old(n):
     print(reduce(lambda a,b: a+b, range(0, n+1)))
 
-sumn(8)
+def sumn(n):
+    if(n > 0):
+        return n + sumn(n-1)
+    return 0
+
+print(sumn(8))
