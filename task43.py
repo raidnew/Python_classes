7a8052207adf250cef12c8984ae3c4cb3d9c8bb9
user <sdaf@sddfgsad.sadf>
2023-04-25 23:14:01 +0300

    *fix task 43

--- task43.py
+++ task43.py
@@ -1,24 +1,31 @@
 #todo:
 # Каждый третий четверг каждого месяца билеты в Эрмитаж бесплатны. Напечатайте список дат в 2023 году, когда вход бесплатен.
 import itertools
-from calendar import monthrange
+import calendar
+import locale
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
 COUNT_MONTH = 12
 COUNT_DAYS_IN_WEEK = 7
-FREE_DAY_INDEX = 3
+FREE_DAY_INDEX = 4
 FREE_DAY_NUM = 3
 
 def get_free_date(check_year = 2023):
     free_date_lst = []
     for month in range(COUNT_MONTH):
         day_count = 0
-        for day in range(monthrange(check_year, month + 1)[1]):
-            if((day + monthrange(check_year, month + 1)[0]) % COUNT_DAYS_IN_WEEK == FREE_DAY_INDEX):
+        for day in range(calendar.monthrange(check_year, month + 1)[1]):
+            if((day + calendar.monthrange(check_year, month + 1)[0]) % COUNT_DAYS_IN_WEEK == FREE_DAY_INDEX):
                 day_count += 1
-                print(day,"  ",(day + monthrange(check_year, month + 1)[0]) % COUNT_DAYS_IN_WEEK)
-                if(day_count == 2):
+                #print(str(day),"  ",str((day + monthrange(check_year, month + 1)[0]) % COUNT_DAYS_IN_WEEK)+"  "+str(day_count))
+                if(day_count == FREE_DAY_NUM):
                     free_date_lst.append((month, day))
     return free_date_lst;
 
-print(get_free_date(2023))
+months = [name for name in calendar.month_name if name]
+
+free_dates = get_free_date(2023)
+
+for date in free_dates:
+    print("%s %d" % (months[date[0]], date[1]))
 
